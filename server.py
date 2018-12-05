164366f0cb2b6476eed9e60e87aa11a034e44eee
thaegis <45537322+thaegis@users.noreply.github.com>
2018-12-05 13:28:44 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,18 @@
 import socket
 import time
 import threading
+
+class encryption():
+	def encrypt:
+		pass
+
+	def decrypt:
+		pass
+
+
+class InT(threading.Thread):
+	def init(self):
+		pass
+
+	def connect(self):
+		pass
